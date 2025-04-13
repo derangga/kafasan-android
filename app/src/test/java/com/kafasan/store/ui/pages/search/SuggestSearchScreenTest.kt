943ed422b73a0728b3e282c7885033d0b31ce72d
dimasrangga <derangga1011@gmail.com>
2025-04-13 18:27:22 +0700

    add suggestion test more usecase

--- app/src/test/java/com/kafasan/store/ui/pages/search/SuggestSearchScreenTest.kt
+++ app/src/test/java/com/kafasan/store/ui/pages/search/SuggestSearchScreenTest.kt
@@ -1,13 +1,22 @@
 package com.kafasan.store.ui.pages.search
 
+import android.content.Context
+import androidx.compose.material3.Text
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsFocused
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performImeAction
 import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.requestFocus
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.createGraph
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
 import com.kafasan.store.data.Product
 import com.kafasan.store.domain.network.Result
 import com.kafasan.store.domain.network.StoreRepository
@@ -69,12 +78,28 @@ class SuggestSearchScreenTest {
                 Product(2, "Product 2", "product-2", 150, "Desc 2", null, emptyList(), "", "")
             )
         )
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val navController = TestNavHostController(context).apply {
+            navigatorProvider.addNavigator(ComposeNavigator())
+            setGraph(createGraph(startDestination = "suggest-search") {
+                composable("suggest-search") { }
+                composable("product/{id}") {
+                    Text("Product Detail Screen")
+                }
+                composable("search/{query}") {
+                    Text("Search Screen")
+                }
+            }, startDestinationArgs = null)
+            setCurrentDestination("suggest-search")
+        }
+
         composeTestRule.setContent {
-            val navController = rememberNavController()
             SuggestSearchScreen(navController, viewModel)
         }
+
         composeTestRule.waitForIdle()
 
+        // assert searchbar and perform typing
         composeTestRule.onNodeWithTag("searchBar")
             .assertExists()
             .assertIsDisplayed()
@@ -85,10 +110,18 @@ class SuggestSearchScreenTest {
         composeTestRule.onNodeWithText("hello world")
             .assertIsDisplayed()
             .requestFocus()
+            .performImeAction()
+
+        // assert goto search screen when tap ime search
+        assert(navController.currentBackStackEntry?.destination?.route?.startsWith("search/") == true)
 
         composeTestRule.waitForIdle()
 
         composeTestRule.onNodeWithText("Product 1").assertIsDisplayed()
         composeTestRule.onNodeWithText("Product 2").assertIsDisplayed()
+            .performClick()
+
+        // assert to product detail
+        assert(navController.currentBackStackEntry?.destination?.route?.startsWith("product/") == true)
     }
 }
